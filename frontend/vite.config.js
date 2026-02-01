a1e81abf9366d8ddc99f703c4f77cb90aeed3526
Steven Illg <bztyla@hotmail.de>
2026-02-01 17:53:53 +0100

    Update base path in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',          // <-- WICHTIG für chijzay.github.io (ohne /repo/)
+  base: '/',
   plugins: [react()],
 })
